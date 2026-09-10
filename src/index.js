4e68304030f29f4801be61fe71783248b2bfe855
Unmiped <famyzstudio@gmail.com>
2026-09-10 09:08:32 +0100

    Added input validation and custom message colors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 const readline = require('readline');
+const colors = {
+    reset: "\x1b[0m",
+    bold: "\x1b[1m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    cyan: "\x1b[36m"
+};
 
 function createStudent(firstName, id, grades) {
     return {
@@ -15,7 +24,6 @@ const melly = createStudent('Melly', 4, [72, 67, 69]);
 
 const students = [zak, med, alive, melly];
 // const students = [];
-
 
 function calculateAverage(std) {
     if(std.grades.length === 0) {
@@ -38,13 +46,6 @@ function findStudentById(id) {
     return null;
 }
 
-// const student = findStudentById(3);
-// if(student != null) console.log('Found student with the name: ' + student.firstName);
-// else console.log('Student not found.');
-
-// const results = showInfo();
-// console.log(results);
-
 function addGradeToStudent(id, value) {
     const student = findStudentById(id);
     if (student == null) {
@@ -52,7 +53,7 @@ function addGradeToStudent(id, value) {
         } 
     else {
             student.grades.push(value); 
-            console.log('Added grade ' + value + ' to ' + student.firstName);
+            console.log(`${colors.green}Added grade ${value} to ${student.firstName}!${colors.reset}`);
         }
 }
 
@@ -61,15 +62,13 @@ function showStudentInfo(id) {
     const student = findStudentById(id);
 
     if (student == null) {
-            console.log('Student not found.');
+            console.log(`${colors.red}Student not found.${colors.reset}`);
         } 
     else {
             const studentAvg = calculateAverage(student);
-            console.log('Name: ' + student.firstName + ' - ID: ' + student.id + ' - Grades: ' + student.grades + ' - Average: ' + studentAvg);
+            console.log(`${colors.blue}First name: ${colors.reset}` + student.firstName + `${colors.blue} ID: ${colors.reset}` + student.id + `${colors.blue} Grades: ${colors.reset}` + student.grades + `${colors.blue} Average: ${colors.reset}` + studentAvg);
         }
 }
-
-// showStudentInfo(3);
 
 function showAllStudents() {
     if(students.length === 0) {
@@ -78,22 +77,26 @@ function showAllStudents() {
     }
     for (const student of students) {
         const studentAvg = calculateAverage(student);
-        console.log(student.firstName + ' - ID: ' + student.id + ' - Grades: ' + student.grades + ' - Average: ' + studentAvg);
+        console.log(`${colors.blue}First name: ${colors.reset}` + student.firstName + `${colors.blue} ID: ${colors.reset}` + student.id + `${colors.blue} Grades: ${colors.reset}` + student.grades + `${colors.blue} Average: ${colors.reset}` + studentAvg);
     }
 }
-
-// showAllStudents();
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-console.log('==== Student Grade Tracker ====\n1. Show all students\n2. Find a student\n3. Add a grade\n4. Exit');
+console.log(`${colors.blue}--------- Student Grade Tracker ---------${colors.reset}`);
+console.log('1. Show all students\n2. Find a student\n3. Add grade to student');
+console.log(`${colors.red}4. Exit${colors.reset}`);
 showMenu();
 
 function showMenu() {
     rl.question('Choose an option: ', (answer) => {
+        if(answer > 4 || answer <= 0) {
+            console.log(`${colors.red}Invlid index.${colors.reset}`);
+            showMenu();
+        }
         if(answer == 1) {
             showAllStudents();
             showMenu();
@@ -109,14 +112,24 @@ function showMenu() {
             rl.question('Student ID? ', (stdID) => {
                 const id = Number(stdID);
                 if(findStudentById(id) == null) {
-                    console.log('Invlid student ID.');
+                    console.log(`${colors.red}Invlid student ID.${colors.reset}`);
                     showMenu();
                 }
                 rl.question('Grade? ', (stdGrade) => {
                     const id = Number(stdID);
                     const grade = Number(stdGrade);
-                    addGradeToStudent(id, grade);
-                    showMenu();
+                    if(stdGrade > 100) {
+                        console.log(`${colors.red}Cannot add grade higher than 100.${colors.reset}`);
+                        showMenu();
+                    }
+                    else if(stdGrade < 0) {
+                        console.log(`${colors.red}Cannot add grade lower than 0.${colors.reset}`);
+                        showMenu();
+                    }
+                    else {
+                        addGradeToStudent(id, grade);
+                        showMenu();
+                    }
             })
             })
         }
